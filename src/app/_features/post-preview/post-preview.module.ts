d57f57491c6f3597e4dcb821b1564a9054e108f5
shavipathania <pathania.shavi@gmail.com>
2019-05-10 14:53:56 +0530

    2 files modified, 1 added, 2 deleted

--- src/app/_features/post-preview/post-preview.module.ts
+++ src/app/_features/post-preview/post-preview.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostPreviewComponent } from './post-preview.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+  ],
+  exports: [
+    PostPreviewComponent
   ],
   declarations: [PostPreviewComponent]
 })
