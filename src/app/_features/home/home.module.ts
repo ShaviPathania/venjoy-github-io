8290e87b0623875fe0565c474da7627f3bb6734b
shavipathania <pathania.shavi@gmail.com>
2019-05-11 08:00:30 +0530

    2 files modified, 8 added

--- src/app/_features/home/home.module.ts
+++ src/app/_features/home/home.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { PostsModule } from '../_post/posts/posts.module';
+import { PageHeaderModule } from '../_page/page-header/page-header.module';
+import { PageFooterModule } from '../_page/page-footer/page-footer.module';
 
 @NgModule({
   imports: [
     CommonModule,
     PostsModule,
     HttpClientModule,
+    PageHeaderModule,
+    PageFooterModule,
   ],
   declarations: [HomeComponent]
 })
