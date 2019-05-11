4c47368295a0c4c6dbe1180851ff8e6c3f7c40f3
shavipathania <pathania.shavi@gmail.com>
2019-05-11 10:41:28 +0530

    4 files modified, 1 added, 1 deleted

--- src/app/_features/_page/page-header/page-header.module.ts
+++ src/app/_features/_page/page-header/page-header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from './page-header.component';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
   ],
   exports: [
     PageHeaderComponent,
