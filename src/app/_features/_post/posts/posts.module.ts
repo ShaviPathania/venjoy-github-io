10026a0cf4050e6098a9f0bdb6d00b23d71d6230
shavipathania <pathania.shavi@gmail.com>
2019-05-11 17:24:08 +0530

    3 files modified, 4 added

--- src/app/_features/_post/posts/posts.module.ts
+++ src/app/_features/_post/posts/posts.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts.component';
 import { PostPreviewModule } from '../post-preview/post-preview.module';
+import { LoadingModule } from '../../loading/loading.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    PostPreviewModule
+    PostPreviewModule,
+    LoadingModule,
   ],
   exports: [
     PostsComponent
