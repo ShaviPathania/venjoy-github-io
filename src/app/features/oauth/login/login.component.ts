1447258ddb25e6fdfeb0d594b7b2dfb8708356ab
shavipathania <pathania.shavi@gmail.com>
2019-05-21 00:24:47 +0530

    1 file modified

--- src/app/features/oauth/login/login.component.ts
+++ src/app/features/oauth/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  hide = t;
+  hide = true;
 
   error = '';
   username = new FormControl('', [Validators.required]);
